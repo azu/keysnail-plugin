44cc8263070e92a3e8db14c9598d390c9ee70814
azu <azuciao@gmail.com>
2013-12-03 21:40:50 +0900

    update for GINZA.
    fix flvplayer name.

--- nicontroller.ks.js
+++ nicontroller.ks.js
@@ -202,7 +202,7 @@ NicoPlayerController.prototype = {
         WATCH_URL:      '^http://[^.]+\.nicovideo\.jp/(watch|playlist)/',
         WATCH_PAGE:     1,
 
-        FLVPLAYER_NODE_ID: 'flvplayer',
+        FLVPLAYER_NODE_ID: 'external_nicoplayer',
 
         STATE_PLAYING:  'playing',
         PLAY:           true,
